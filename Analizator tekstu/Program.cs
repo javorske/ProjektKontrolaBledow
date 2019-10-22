ff0b8d7890b3951edcc869b39ba01dd67ae5b2c6
Jakub <jakubniemam@yahoo.com>
2019-10-22 21:08:34 +0200

    Added new option which will allow to download file from server. I used for this 2 new props and 1 new fuction which is using WebClient

--- Analizator tekstu/Program.cs
+++ Analizator tekstu/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +9,20 @@ namespace Analizator_tekstu
 {
     class Program
     {
+        /// <summary>
+        /// The url path to file.
+        /// </summary>
+        public static string urlPath { get; set; } = "https://s3.zylowski.net/public/input/1.txt";
+        /// <summary>
+        /// The name of file.
+        /// </summary>
+        public static string filePath { get; set; } = "File.txt";
         static void Main(string[] args)
         {
             int option = 0; //it needs to be assigned from the beginnig
             bool checkForEnding = true;
             bool checkForException = true;
+
             while (checkForEnding)
             {
                 // menu displayed in console
@@ -45,10 +55,11 @@ namespace Analizator_tekstu
                         Console.WriteLine("Nieprawidłowy format bądź liczba z poza menu. Prosze sprobowac jeszcze raz.");
                     }
                 } while (checkForException);
-                
+
                 switch (option)
                 {
                     case 1:
+                        getFileFromInternet(urlPath, filePath);
                         break;
                     case 2:
                         break;
@@ -67,6 +78,17 @@ namespace Analizator_tekstu
                         break;
                 }
             }
+
+        }
+        /// <summary>
+        /// Download and save file form given url to given file name
+        /// </summary>
+        /// <param name="urlPath"></param>
+        /// <param name="fileName"></param>
+        public static void getFileFromInternet(string urlPath, string fileName)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(urlPath, fileName);
         }
     }
 }
